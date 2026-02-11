f82b9fd06a21ed6727c02b78f2f9c96f51ecda50
LucasHNascimento <130712881+LucasHNascimento@users.noreply.github.com>
2026-02-11 23:21:38 +0000

    Primeiro patch teste

---
+++ aula00-conteudo/ex01/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import OlaMundo from "./components/OlaMundo";
+
+export default function App(){
+  return(
+    <View style = {styles.container}>
+      <OlaMundo nome = 'Lucas'/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f57',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
